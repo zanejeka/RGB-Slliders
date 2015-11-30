96b2e3202f68df1931f10876d163426b8561b814
cheryl springfels <cheryl@springfels.com>
2015-11-30 10:32:27 -0500

    Finalized project

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet var colorSquare : UIView?
 
+    
+   
+        // Do any additional setup after loading the view, typically from a nib.
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorSquare!.layer.borderColor = UIColor.blackColor().CGColor
+        colorSquare!.layer.borderWidth = 1
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateBackgroundColor()
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        redSlider.value = defaults.floatForKey("red")
+        greenSlider.value = defaults.floatForKey("green")
+        blueSlider.value = defaults.floatForKey("blue")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        // view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        self.colorSquare!.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        let defaults = NSUserDefaults.standardUserDefaults()  //1
+        defaults.setFloat(redSlider.value, forKey: "red")   //2
+        defaults.setFloat(greenSlider.value, forKey: "green")
+        defaults.setFloat(blueSlider.value, forKey: "blue")
+        defaults.synchronize()
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "openColor") {
+            let newViewController = segue.destinationViewController
+            newViewController.view.backgroundColor = colorSquare!.backgroundColor
+        }
+    }
+    
+    
 }
 
